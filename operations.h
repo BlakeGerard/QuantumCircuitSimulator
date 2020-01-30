a705644e3ec6b3531d317a3320f415f5d35eb8fe
BlakeGerard <blakegerard@ou.edu>
2020-01-30 11:24:44 +0000

    Removed old storage files

--- operations.h
+++
@@ -1,3 +0,0 @@
-#include "qubit.h"
-
-Eigen::Vector4cd bell_state_entanglement(Qubit *control, Qubit *target);
--- q_gate.h
+++
@@ -1,9 +0,0 @@
-#ifndef Q_GATE_H
-#define Q_GATE_H
-#include "qubit.h"
-#include </usr/include/eigen3/unsupported/Eigen/KroneckerProduct>
-
-void hadamard(Qubit *qubit);
-Eigen::Vector4cd cnot(Qubit *control, Qubit *target);
-
-#endif
